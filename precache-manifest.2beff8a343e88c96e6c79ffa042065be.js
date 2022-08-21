b8adaffa9f3b28dbb963bc0ec0621cd7ecc4b13c
sakshisubedi <35215747+sakshisubedi@users.noreply.github.com>
2022-08-21 16:41:46 +0000

    Deploying to gh-pages from  @ f5100706e7a40eca0a4b2810654c4cbefb164c85 🚀

---
+++ precache-manifest.2beff8a343e88c96e6c79ffa042065be.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774f4cd1b261bc9fe8097e2e76d1fb50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b168ec81a76ed366262",
+    "url": "/static/css/3.82008174.chunk.css"
+  },
+  {
+    "revision": "fe4addb1303b898222d3",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "9db96cab20086794887b",
+    "url": "/static/css/main.4cbd0349.chunk.css"
+  },
+  {
+    "revision": "6a9e5fa6cb1ebc277763",
+    "url": "/static/js/2.7eeee9a9.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/static/js/2.7eeee9a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b168ec81a76ed366262",
+    "url": "/static/js/3.4da6b6e0.chunk.js"
+  },
+  {
+    "revision": "fe4addb1303b898222d3",
+    "url": "/static/js/4.2207e6e7.chunk.js"
+  },
+  {
+    "revision": "9db96cab20086794887b",
+    "url": "/static/js/main.f4cf3463.chunk.js"
+  },
+  {
+    "revision": "08b7f3b0a8fec45ed637",
+    "url": "/static/js/runtime-main.b02258ef.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "2695e2466148dd6c6bfe220b03708952",
+    "url": "/static/media/muLogo.2695e246.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "05cd336e1ef4554ac21e6c76fed466dd",
+    "url": "/static/media/ucsdLogo.05cd336e.png"
+  }
+]);
